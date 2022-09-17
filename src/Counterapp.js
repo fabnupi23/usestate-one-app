20fe3b1ceadd6a6262a03abc5146e7e479f588c2
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2022-09-17 12:56:16 -0500

    Finaliza el ejercicio incremental usando useState

--- src/Counterapp.js
+++ src/Counterapp.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Counterapp = () => {
+    const [counter, setCounter] = useState(0);
+
+
+    //Función que se encarga de incrementar
+    const incremetnCounter = () => {
+        setCounter(counter+1);
+    }
+
+
   return (
     <div>
-        <h1>Counterapp</h1>
+        <h1>Clicks {counter}</h1>
+        <button onClick={incremetnCounter}>Incrementar</button>
     </div>
   )
 }
